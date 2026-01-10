d56429def54156fc99ce83ff013ca28cc02079dc
Sanika Surose <109695485+sanikasurose@users.noreply.github.com>
2026-01-10 14:47:57 -0500

    Set up XCode project

---
+++ clAIrity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clAIrity
+//
+//  Created by Sanika Surose on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clAIrity/clAIrityApp.swift
@@ -0,0 +1,17 @@
+//
+//  clAIrityApp.swift
+//  clAIrity
+//
+//  Created by Sanika Surose on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct clAIrityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
